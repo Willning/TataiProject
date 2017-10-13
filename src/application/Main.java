301e8796b1e3038fe382d2ed1f0ed008d71f8dae
Victoria Skeggs <vske511@en-369738.uoa.auckland.ac.nz>
2017-10-13 21:15:40 +1300

    Added quit, game feedback and custom list screens

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
