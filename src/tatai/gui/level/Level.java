0f75eeb87ff6e20d5abc83c4bb6b683ab5797caf
William Ning <12452williamning@gmail.com>
2017-10-22 08:49:03 +1300

    messed around with GUI, added a timer for recordings

--- src/tatai/gui/level/Level.java
+++ src/tatai/gui/level/Level.java
@@ -1,9 +1,10 @@
 package tatai.gui.level;
 
 
-import javafx.animation.Animation;
-import javafx.animation.Interpolator;
-import javafx.animation.Transition;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.animation.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -32,12 +33,15 @@ public class Level implements Initializable{
     AnchorPane anchorPane;
 
     @FXML
-    Label equationText, roundText;
+    Label equationText, roundText, currentScore, questionsLeft, statusLabel;
 
     @FXML
     Button backButton, recordButton, continueButton;
 
+
     private Game game;
+    private Integer RECORDTIME = 3;
+    Integer timerStart = RECORDTIME;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -60,8 +64,29 @@ public class Level implements Initializable{
     public void recordHit() {
         recordButton.setDisable(true);
         System.out.println("recording....");
+        recordCountdown();
         game.record();
     }
+
+    //method so that once the record button is hit, an approritate coutndown for the recording begins.
+    private void recordCountdown(){
+        statusLabel.setText("Recording: " + timerStart.toString());
+
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add
+                (new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                       timerStart--;
+                       statusLabel.setText("Recording: " + timerStart.toString());
+                       if (timerStart<=0){
+                           timeline.stop();
+                       }
+                    }
+                }));
+        timeline.playFromStart();
+    };
 
     public void recordingDone() {
         System.out.println("processing...");
@@ -81,12 +106,10 @@ public class Level implements Initializable{
 
     public void answerCorrect() {
         final Animation animation = new Transition() {
-
             {
                 setCycleDuration(Duration.millis(1000));
                 setInterpolator(Interpolator.EASE_OUT);
             }
-
             @Override
             protected void interpolate(double frac) {
                 Color vColor = new Color(0.5, 1, 0, 1 - frac);
@@ -94,13 +117,15 @@ public class Level implements Initializable{
             }
         };
         animation.play();
+
+        statusLabel.setText("Correct the answer was " + game.getReceivedAnswer());
+
         System.out.println("Correct");
         System.out.println("We received: " + game.getReceivedAnswer());
     }
 
     public void answerWrong() {
         final Animation animation = new Transition() {
-
             {
                 setCycleDuration(Duration.millis(1000));
                 setInterpolator(Interpolator.EASE_OUT);
@@ -113,6 +138,9 @@ public class Level implements Initializable{
             }
         };
         animation.play();
+
+        statusLabel.setText("Sorry, that was not correct.");
+
         System.out.println("Incorrect");
         System.out.println("We received: " + game.getReceivedAnswer());
     }
