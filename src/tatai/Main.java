6dcecdbde6e587fa4aac41f03dd9ceaf2ec7ac30
Winston Zhao <winstonzhao@outlook.co.nz>
2017-10-27 02:24:38 +1300

    Some testing

--- src/tatai/Main.java
+++ src/tatai/Main.java
@@ -4,26 +4,30 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import tatai.gui.customListSelect.CustomListView;
-import tatai.gui.customSelectScreen.CustomSelectController;
-import tatai.gui.customSelectScreen.CustomSelectView;
 import tatai.gui.welcomeScreen.WelcomeScreen;
 import tatai.gui.welcomeScreen.WelcomeScreenView;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        // Load the font
+        Font.loadFont(Main.class.getResource("Schoolbell.ttf").toExternalForm(), 10);
+
         WelcomeScreenView welcomeScreenView = new WelcomeScreenView();
-        Parent root = (Parent)welcomeScreenView.view();
-        WelcomeScreen welcomeScreen = (WelcomeScreen)welcomeScreenView.controller();
-    	welcomeScreen.setUpStage(primaryStage);
-    	primaryStage.setTitle("Tatai!");
+        Parent root = (Parent) welcomeScreenView.view();
+        WelcomeScreen welcomeScreen = (WelcomeScreen) welcomeScreenView.controller();
+        welcomeScreen.setUpStage(primaryStage);
+        primaryStage.setTitle("Tatai!");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.getIcons().add(new Image("resources/images/maori.png"));
     }
+
 
 
     public static void main(String[] args) {
